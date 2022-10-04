71016509325b45c261268cbd90fe4c0cd67f5f20
Douglas Gabriel Cardoso <douglasdm97@gmail.com>
2022-10-04 02:20:18 -0300

    fix(infrastructure): build pipeline

--- integration/hello-world/infrastructure.ts
+++ integration/hello-world/infrastructure.ts
@@ -6,8 +6,6 @@ export class HelloWorldStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // console.log('atlasKey', atlasKey);
-
     // 👇 define GET hello world function
     const helloWorldLambda = new lambda.Function(this, 'HelloHandler', {
       runtime: lambda.Runtime.NODEJS_14_X,
