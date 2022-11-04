2f3635162015da8c5126cb166d401725e15851d9
Douglas Gabriel Cardoso <32394842+douglasgc@users.noreply.github.com>
2022-11-04 18:10:16 -0300

    Cicd/multiples integrations pipelines (#4)
    * test integration deploy pipe
    
    * test integration deploy pipe
    
    * test integration hello world pipeline
    
    * test integration hello world pipeline
    
    * dependency cache add modules lock files
    
    * add aws cli environment setup
    
    * remove install for test
    
    * remove re-build step
    
    * optimize pipeline with artifacts
    
    * optimize pipeline build packages
    
    * cache integration dist
    
    * cache integration dist

--- scripts/core.sh
+++ scripts/core.sh
@@ -1,6 +1,4 @@
 COMMAND=$1
 SERVICE=$2
 STACK_LAYER=$3
-npm run build:packages
-npm run build:integration
 npx cdk $COMMAND --app "node integration_dist/${SERVICE}/infrastructure.js build ${STACK_LAYER}" --all $4 $5
