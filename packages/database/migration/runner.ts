b8c1bdad3d95b823bf28b8a5e34c4f7f82144581
Douglas Gabriel Cardoso <douglasdm97@gmail.com>
2022-12-27 13:45:57 -0300

    feat(database): add database command proxy

--- packages/database/migration/runner.ts
+++ packages/database/migration/runner.ts
@@ -1,12 +1,18 @@
 import { promisify } from 'util';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const exec = promisify(require('child_process').exec);
 
-export const handler = async ({ command }: any,) => {
-    try {
-        const { stdout, stderr } = await exec(command);
-        console.log('stdout:', stdout);
-        console.log('stderr:', stderr);
-    } catch (e) {
-        console.error(e);
-    }
+export const handler = async ({ command }: any) => {
+  try {
+    const { stdout, stderr } = await exec(command);
+    console.log('stdout:', stdout);
+    console.log('stderr:', stderr);
+    return {
+      stderr,
+      stdout,
+    };
+  } catch (e) {
+    console.error(e);
+    throw new e();
+  }
 };
